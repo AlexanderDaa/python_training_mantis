af26d73fc5a0b6b8abc30ccb538c01053df37e88
AlexanderDaa <12345qazWS>
2019-03-09 21:21:09 +0600

    видео 9_5

--- fixture/signup.py
+++ fixture/signup.py
@@ -1,4 +1,5 @@
 import re
+import time
 
 class SignupHelper:
 
@@ -10,15 +11,20 @@ class SignupHelper:
         wd.get(self.app.base_url + "signup_page.php")
         wd.find_element_by_name("username").send_keys(username)
         wd.find_element_by_name("email").send_keys(email)
+        time.sleep(2)
         wd.find_element_by_css_selector("input[type='submit']").click()
-
 
         mail = self.app.mail.get_mail(username,password,"[MantisBT] Account registration")
         url = self.extract_confirmation_url(mail)
 
         wd.get(url)
         wd.find_element_by_name("password").send_keys(password)
+        time.sleep(1)
         wd.find_element_by_name("password_confirm").send_keys(password)
+        time.sleep(1)
+        wd.find_element_by_name("password").clear
+        wd.find_element_by_name("password").send_keys(password)
+        time.sleep(1)
         wd.find_element_by_css_selector("input[value='Update User']").click()
 
     def extract_confirmation_url(self,text):
--- test/test_signup_new_account.py
+++ test/test_signup_new_account.py
@@ -11,9 +11,11 @@ def random_string(prefix, maxlen):
 def test_sign_up_new_account(app):
     username = random_string("user_",10)
     password = "test"
-    email =username+"@localhost"
+    email = username+"@localhost"
     app.james.ensure_user_exists(username, password)
     app.signup.new_user(username,email,password)
-    app.session.login(username,password)
-    assert app.session.is_logged_in_as(username )
-    app.session.logout()
+    #app.session.login(username,password)
+    #assert app.session.is_logged_in_as(username )
+    #app.session.logout()
+    print(username+" - "+password)
+    assert app.soap.can_login(username,password)
